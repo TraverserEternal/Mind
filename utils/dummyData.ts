41e97ae62499968baa7fbbeb1f129d2fdd73ea37
Kimball <kimball.bradford@gmail.com>
2023-09-24 01:31:46 +0200

    Added Dummy Data

---
+++ utils/dummyData.ts
@@ -0,0 +1,104 @@
+import Day from "../declarations/Day";
+
+const daysData: Day[] = [
+  {
+    date: new Date("2023-09-24T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2023-09-24T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag1", "Tag4"],
+      },
+    ],
+  },
+  {
+    date: new Date("2023-11-24T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2023-11-24T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag3"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-01-23T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-01-23T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag2", "Tag4", "Tag5"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-03-23T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-03-23T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag1", "Tag3"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-05-22T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-05-22T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag2", "Tag5"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-07-21T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-07-21T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag3"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-09-19T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-09-19T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag1", "Tag4", "Tag5"],
+      },
+    ],
+  },
+  {
+    date: new Date("2024-11-18T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2024-11-18T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag2"],
+      },
+    ],
+  },
+  {
+    date: new Date("2025-01-17T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2025-01-17T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag1", "Tag3", "Tag5"],
+      },
+    ],
+  },
+  {
+    date: new Date("2025-03-18T10:00:00.000Z"),
+    entries: [
+      {
+        timestamp: new Date("2025-03-18T10:00:00.000Z"),
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tags: ["Tag4"],
+      },
+    ],
+  },
+];
