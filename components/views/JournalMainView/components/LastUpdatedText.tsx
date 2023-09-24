de76ff6e45f9aa2920b3dfa9e03c1a48dd468e4e
Kimball <kimball.bradford@gmail.com>
2023-09-24 13:20:09 +0200

    Updated LastUpdatedText to be more precise

--- components/views/JournalMainView/components/LastUpdatedText.tsx
+++ components/views/JournalMainView/components/LastUpdatedText.tsx
@@ -1,10 +1,9 @@
 import moment from "moment";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef } from "react";
 import { Animated, TextProps } from "react-native";
 import { useJournalContext } from "../../../../hooks/useJournalData";
 
 const LastUpdatedText: FC<TextProps> = ({ style, ...props }) => {
-  const [text, setText] = useState("");
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const { lastUpdated } = useJournalContext();
   useEffect(() => {
@@ -17,28 +16,13 @@ const LastUpdatedText: FC<TextProps> = ({ style, ...props }) => {
       }),
     ]).start();
   }, [fadeAnim]);
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout;
-    setText("Saved!");
-    const timeoutID = setTimeout(() => {
-      setText(getLastUpdatedTextData());
-      intervalId = setInterval(() => setText(getLastUpdatedTextData()), 30000);
-    });
-    return () => {
-      if (timeoutID !== null) clearTimeout(timeoutID);
-      if (intervalId !== null) clearInterval(intervalId);
-    };
-  }, [lastUpdated]);
-  const getLastUpdatedTextData = (): string => {
-    if (!lastUpdated) return "Last Saved: Never";
-    if (new Date().toDateString() === lastUpdated.toDateString()) {
-      return `Last Saved: ${moment(lastUpdated).fromNow()}`;
-    }
-    return `Last Saved: ${lastUpdated.toDateString()} at ${lastUpdated.toLocaleTimeString()}`;
-  };
   return (
     <Animated.Text style={[style, { opacity: fadeAnim }]} {...props}>
-      {text}
+      {lastUpdated == null
+        ? "Last Saved: Never"
+        : `Saved on ${lastUpdated.toDateString()} at ${lastUpdated.toLocaleTimeString()}\n(${moment(
+            lastUpdated
+          ).fromNow()})`}
     </Animated.Text>
   );
 };
